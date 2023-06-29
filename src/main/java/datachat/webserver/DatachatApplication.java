56a3fdfdf1f20ecb8ec4949cec55a54a1df90fea
Michał Słapek <28485371+mslapek@users.noreply.github.com>
2023-06-29 20:13:47 +0100

    Initial commit

---
+++ src/main/java/datachat/webserver/DatachatApplication.java
@@ -0,0 +1,13 @@
+package datachat.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatachatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatachatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/datachat/webserver/DatachatApplicationTests.java
@@ -0,0 +1,13 @@
+package datachat.webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatachatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
